505c8c6c6f0ca2ab27faacded329819d9b3cc52c
TalonDragon000 <88998252+TalonDragon000@users.noreply.github.com>
2024-11-11 00:47:12 -1000

    Update vite.config.js
    Added local host and explicit port for debugging / testing

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,9 +2,16 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
-    open: true, // Automatically open the browser on server start
+    open: true,
+    port: 5174, // Explicitly set port
+    proxy: {
+      '/socket.io': {
+        target: 'http://localhost:3000',
+        ws: true
+      }
+    }
   },
   build: {
-    outDir: "dist", // Output directory for the build
+    outDir: "dist",
   },
 });
